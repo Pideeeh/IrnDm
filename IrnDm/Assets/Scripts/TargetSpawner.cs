aa9cb444c803d2c4cc3f02ead9eaf38d599bd47a
VerticalVodka <markus.maureder.mm@gmail.com>
2017-06-10 16:21:22 +0200

    Spawner
    Spawner
    Change FirstPerson Start Position

--- IrnDm/Assets/Scripts/TargetSpawner.cs
+++ IrnDm/Assets/Scripts/TargetSpawner.cs
@@ -29,10 +29,17 @@ public class TargetSpawner : MonoBehaviour {
     IEnumerator MakeObject()
     {
         readyNow = false;
-        Instantiate(Target, NextSpawnPosition());
-        yield return new WaitForSeconds(SpawnSpeed);
+        //Debug.Log("Spawn");
+        int SpawnDistance = Random.Range(15, 30);
+        float SpawnAngel = NDistribution(0, Mathf.PI / 2);
+        float FacingAngel = Random.Range(0.7f, 1.2f);
+        Vector3 SpawnDirection = new Vector3(Mathf.Cos(SpawnAngel), 0, Mathf.Sin(SpawnAngel));
+        //int rotation = (int)(Mathf.Sign(SpawnDirection.x) * Mathf.Sign(SpawnDirection.z));
+        Instantiate(Target, SpawnDirection * SpawnDistance + new Vector3(0, 4, 0), new Quaternion(SpawnDirection.z, 0, -SpawnDirection.x, Mathf.Cos(FacingAngel)));
+        yield return new WaitForSecondsRealtime(SpawnSpeed);
         readyNow = true;
     }
+
 
     Transform NextSpawnPosition() {
         //Add logic for random spawn
@@ -46,5 +53,21 @@ public class TargetSpawner : MonoBehaviour {
 
     public void StopSpawning() {
         isSpawning = false;
+    }
+
+    public float NDistribution(float mean, float std)
+    {
+        float u1 = 0;
+        float u2 = 0;
+        float v = 0;
+
+        while (v > 1 || v == 0)
+        {
+            u1 = Random.Range(-1f, 1f);
+            u2 = Random.Range(-1f, 1f);
+            v = u1 * u1 + u2 * u2;
+        }
+
+        return mean + std * v;
     }
 }
